fdb4398621afd0d5a0cc4f7a005d0ac73d0387a0
Reid Sherman <reidsherman@gmail.com>
2019-11-21 15:25:26 -0800

    Fix seo

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import SEO from "../components/Seo"
 
 const About = () => (
   <Layout>
-  <h1>About</h1>
-  <h4>TODO: Who built this site? (CfA SF Brigade, tech equity collab, community legal services east palo alto, pics/etc</h4>
+    <SEO title="About" />
+    <h1>About</h1>
+    <h4>TODO: Who built this site? (CfA SF Brigade, tech equity collab, community legal services east palo alto, pics/etc</h4>
   </Layout>
 )
 
