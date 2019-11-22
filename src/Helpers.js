6ead52d027852cbc40d5a594544ee35261f144af
Reid Sherman <reidsherman@gmail.com>
2019-11-21 21:38:57 -0800

    Add test scenarios

--- src/Helpers.js
+++ src/Helpers.js
@@ -13,28 +13,43 @@ export function calculateMaxRent(pastRent = 0, cpi = 0.033) {
   return parseFloat(min).toFixed(2)
 }
 
+// Used to determine the rent on march 15 2019 from range data
+export function determineRentOnDateFromRentRanges(targetDate, rentRanges = []) {
+  if (!targetDate) throw new Error('No target date given.')
+  let rent
+  for(let i = 0; i < rentRanges.length; i++) {
+    const daysBefore = targetDate.diff(rentRanges[i].startDate, 'days', true)
+    const daysAfter = rentRanges[i].endDate.diff(targetDate, 'days', true)
+    if (daysBefore >= 0 && daysAfter >= 0) rent = rentRanges[i].rent
+  }
+  return rent
+}
+
+export function determineMaxRentFromRentRanges(targetDate, rentRanges = [], cpi = 0.033) {
+  const rentOnTargetDate = determineRentOnDateFromRentRanges(targetDate, rentRanges)
+  return calculateMaxRent(rentOnTargetDate, cpi)
+}
+
 export function calculateTotalAmountOwedToTenant(rentRanges = [], cpi = 0.033) {
   // NOTE: rentRanges must be sorted past --> present
+  // WARNING: Only accurate before mar 15 2020!
   let result = 0
   if (rentRanges.length < 1) return result
-  let pastRent
-  let maxRent
-  
-  for(let i = 0; i < rentRanges.length; i++) {
-    const rent = rentRanges[i].rent
-    const daysBeforeMar152019 = moment([2019, 2, 15]).diff(rentRanges[i].startDate, 'days', true)
-    
-    if (!pastRent && daysBeforeMar152019 >= -1) {
-      // pastRent: the user's rent on march 15 2019
-      pastRent = rent
-      maxRent = calculateMaxRent(pastRent, cpi)
-    } else if (pastRent) {
-      const janFirst2020 = moment([2020, 0, 1])
-      const diff = rentRanges[i].endDate.diff(janFirst2020, 'months', true)
-      const monthsPaidAfterJan2020 = diff > 0 ? diff : 0
-      result += (rent > maxRent) ? (rent - maxRent) * monthsPaidAfterJan2020 : 0
-    }
-  }
+  const mar152019 = moment([2019, 2, 15])
+  const pastRent = determineRentOnDateFromRentRanges(mar152019, rentRanges)
 
+  for (let i = 0; i < rentRanges.length; i++) {
+    const rent = rentRanges[i].rent
+    const start = rentRanges[i].startDate
+    const end = rentRanges[i].endDate
+    
+    const maxRent = calculateMaxRent(pastRent, cpi)
+    const janFirst2020 = moment([2020, 0, 1])
+    const diff = rentRanges[i].endDate.diff(janFirst2020, 'months', true)
+    const isAfterJan2020 = diff > 0 ? true : false
+    const monthsPaidAfterJan2020 = isAfterJan2020 ? end.diff(start, 'months', true) : 0
+
+    result += (rent > maxRent) ? (rent - maxRent) * monthsPaidAfterJan2020 : 0
+  }
   return result > 0 ? parseFloat(result).toFixed(2) : 0
 }
