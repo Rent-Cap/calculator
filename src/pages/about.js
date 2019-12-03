1612273db8208fc684571d3c9b2237b8847f584d
Reid Sherman <reidsherman@gmail.com>
2019-12-03 13:41:29 -0800

    Add copy

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,10 @@ const About = () => (
   <Layout>
     <SEO title="About" />
     <h1>About</h1>
-    <h4>TODO: Who built this site? (CfA SF Brigade, tech equity collab, community legal services east palo alto, pics/etc</h4>
+    <h2>Who built the site</h2>
+    <p>This site was built by the Code For San Francisco Brigade and the Tech Equity Collaborative with legal help from Community Legal Services in East Palo Alto.</p>
+    <h2>How can I contribute</h2>
+    <p>Join the #rentcap channel in the Code For San Francisco slack (<a href="http://c4sf.me">c4sf.me</a>) or make pull requests directly through our <a href="https://github.com/Rent-Cap/">github organization</a></p>
   </Layout>
 )
 
