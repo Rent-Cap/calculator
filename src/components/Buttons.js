1350addaff0b3800cefa9c9489db49c6396f6365
Reid Sherman <reidsherman@gmail.com>
2019-12-06 20:57:28 -0800

    Make hamburger nav menu

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,6 +3,9 @@ import React from 'react'
 export const PrimaryButton = ({onClick, children}) => (
   <button onClick={onClick} className="btn btn-outline-primary">{children}</button>
 )
+export const PrimaryButton2 = ({onClick, children}) => (
+  <button onClick={onClick} className="btn btn-primary">{children}</button>
+)
 export const SecondaryButton = ({onClick, children}) => (
   <button onClick={onClick} className="btn btn-outline-secondary">{children}</button>
 )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,63 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import SEO from "../components/Seo"
 import 'bootstrap/dist/css/bootstrap.css';
 import { PrimaryButton, SecondaryButton } from '../components/Buttons'
 import i18n from '../i18n';
 import { withTranslation } from 'react-i18next';
 import logo from '../images/logo.svg'
-const Header = ({ siteTitle, t }) => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <SEO title="Home" />
-    <Link className="navbar-brand" to="/"><img src={logo}></img></Link>
-    {/* <div className="container"> */}
-    <div className="TODO-Add-Hamburger" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <Link className="nav-item nav-link" to="/eligibility">
-          {t('eligible')}
-        </Link>
-        <Link className="nav-item nav-link" to="/calculator">
-          I'm eligible (Calculator)
-        </Link>
-        {/* <Link to="/resources">
-          Resources
-        </Link> */}
-        <Link className="nav-item nav-link" to="/bill">
-          About The Bill
-        </Link>
-        <Link className="nav-item nav-link" to="/about">
-          About RentCap
-        </Link>
-      </div>
-    </div>
-    {/* </div> */}
-    {/* TODO: Add spanish translation */}
-    {/* <SecondaryButton onClick={() => i18n.changeLanguage('en')}>English</SecondaryButton>
-    <SecondaryButton onClick={() => i18n.changeLanguage('es')}>Espanol</SecondaryButton> */}
-  </nav>
-)
+import hamburger from '../images/hamburger.svg'
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: `Rent Cap`,
-}
+class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      showMenu: false,
+    }
+    this.showNavLinks = this.showNavLinks.bind(this)
+    this.handleOutsideClick = this.handleOutsideClick.bind(this)
+  }
+  showNavLinks(e) {
+    this.setState({ showMenu: true })
+  }
+  handleOutsideClick(e) {
+    this.setState({ showMenu: false})
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div onClick={this.handleOutsideClick} className={`overlay${this.state.showMenu ? ' active' : ''}`}></div>
+        <SEO title="Home" />
+        <Link className={`navbar-brand${this.state.showMenu ? ' hidden' : ''}`} to="/"><img src={logo}></img></Link>
+        {/* <div className="container"> */}
+        <div className={`navlink-group${this.state.showMenu ? ' active' : ''}`}>
+          <div className="navbar-nav">
+            <Link className="nav-item nav-link" to="/bill">
+              What is the Tenant Protection Act?
+            </Link>
+            <Link className="nav-item nav-link" to="/eligibility">
+              Am I Protected
+            </Link>
+            <Link className="nav-item nav-link" to="/calculator">
+              Rent Calculator
+            </Link>
+            <Link className="nav-item nav-link">
+              Resources (Coming Soon)
+            </Link>
+            <Link className="nav-item nav-link" to="/about">
+              About Us
+            </Link>
+          </div>
+        </div>
+        <div className={`hamburger${this.state.showMenu ? ' hidden' : ''}`} onClick={this.showNavLinks}><img src={hamburger}></img></div>
+        {/* </div> */}
+        {/* TODO: Add spanish translation */}
+        {/* <SecondaryButton onClick={() => i18n.changeLanguage('en')}>English</SecondaryButton>
+        <SecondaryButton onClick={() => i18n.changeLanguage('es')}>Espanol</SecondaryButton> */}
+      </nav>
+      )
+  }
+} 
 
 export default withTranslation()(Header)
